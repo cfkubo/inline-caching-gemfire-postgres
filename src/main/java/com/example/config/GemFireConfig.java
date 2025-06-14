5a8da89b697b1b31b5721604fe3aa3288449e809
cfkubo <arul.vannala@gmail.com>
2025-06-14 02:42:45 -0600

    config

--- src/main/java/com/example/config/GemFireConfig.java
+++ src/main/java/com/example/config/GemFireConfig.java
@@ -1,23 +1,71 @@
+// package com.example.config;
+
+// import com.example.model.DataItem;
+// import org.apache.geode.cache.client.ClientCache;
+// import org.apache.geode.cache.client.ClientCacheFactory;
+// import org.apache.geode.cache.client.ClientRegionFactory;
+// import org.apache.geode.cache.client.ClientRegionShortcut;
+// import org.apache.geode.cache.Region;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+
+// @Configuration
+// public class GemFireConfig {
+
+//     @Bean
+//     public ClientCache clientCache() {
+//         return new ClientCacheFactory()
+//                 .setPoolSubscriptionEnabled(true)
+//                 .create();
+//     }
+
+//     @Bean
+//     public Region<String, DataItem> dataItemRegion(ClientCache clientCache) {
+//         ClientRegionFactory<String, DataItem> regionFactory = clientCache.createClientRegionFactory(ClientRegionShortcut.PROXY);
+//         return regionFactory.create("dataItemRegion");
+//     }
+// }
+
 package com.example.config;
 
 import com.example.model.DataItem;
+import com.example.model.Product;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.cache.Region;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.model.DataItem;
 
 @Configuration
 public class GemFireConfig {
+
+    @Value("${app.gemfire.region-name}")
+    private String regionName;
+
+    @Value("${spring.data.gemfire.locator.host:localhost}")
+    private String locatorHost;
+
+    @Value("${spring.data.gemfire.locator.port:10334}")
+    private int locatorPort;
 
     @Bean
     public ClientCache clientCache() {
         return new ClientCacheFactory()
                 .setPoolSubscriptionEnabled(true)
+                .addPoolLocator(locatorHost, locatorPort)
                 .create();
+    }
+
+    @Bean
+    public Region<Integer, Product> productsRegion(ClientCache clientCache) {
+        ClientRegionFactory<Integer, Product> regionFactory = clientCache.createClientRegionFactory(ClientRegionShortcut.PROXY);
+        return regionFactory.create(regionName);
     }
 
     @Bean
